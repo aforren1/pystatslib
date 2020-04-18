3a8c9a54b5e984af597a7c87e30e2a5906523226
Alex Forrence <alex.forrence@gmail.com>
2020-04-18 13:36:05 -0400

    add explicit return (auto wasn't catching bernoulli/binom)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 git clone https://github.com/aforren1/pystatslib --recursive
+
+Check preprocessing: 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+cl /P /C src\rand.cpp
--- src/rand.cpp
+++ src/rand.cpp
@@ -3,9 +3,24 @@
 #define STATS_ENABLE_EIGEN_WRAPPERS
 #include "stats.hpp"
 
+// define all distributions
+#define fnRnorm rnorm
+#define fnRbern rbern
+#define fnRbeta rbeta
+#define fnRbinom rbinom
+#define fnRcauchy rcauchy
+#define fnRchisq rchisq
+#define fnRexp rexp
+
+
 using Mxd = Eigen::MatrixXd;
 using Mxi = Eigen::Matrix<uint32_t, -1, -1>;
 using rand_engine_t = std::mt19937_64;
+
+#define outD double
+#define outI uint32_t
+#define outM Mxd
+#define outMi Mxi
 
 #define declD(arg) const double arg
 #define declI(arg) const uint32_t arg // TODO: check 32 or 64 bit? it's "uint_t" in the docs
@@ -18,25 +33,22 @@ using rand_engine_t = std::mt19937_64;
 #define tempMi <Mxi>
 #define tempN
 #define eleD(arg) arg
+#define eleI(arg) arg
 #define eleSeed(arg) seed_val
 #define eleEng(arg) engine
 #define eleDim(arg) n, k
 #define eleN(arg)
 
 #define commaD ,
+#define commaI ,
 #define commaSeed ,
 #define commaEng ,
 #define commaDim ,
 #define commaN
 
-#define fnRnorm rnorm
-#define fnRbern rbern
-#define fnRbeta rbeta
-#define fnRbinom rbinom
-
 // inspired by https://github.com/kaskr/adcomp/blob/master/TMB/inst/include/Vectorize.hpp
-#define GRAND(FUN, Temp, Type1, Type2, Type3, Type4) \
-auto fn##FUN ( decl##Type1(arg1) comma##Type2 \
+#define GRAND(FUN, Ret, Temp, Type1, Type2, Type3, Type4) \
+out##Ret fn##FUN ( decl##Type1(arg1) comma##Type2 \
              decl##Type2(arg2) comma##Type3 \
              decl##Type3(arg3) comma##Type4 \
              decl##Type4(arg4) ) \
@@ -47,37 +59,40 @@ temp##Temp ( ele##Type1(arg1) comma##Type2 \
            ); } \
 
 // function, return type, template type, arg1 type, arg2 type, arg3 type, arg4 type (seed or rng)
-GRAND(Rnorm, N, N, N, N, N) // special case: rnorm can take no args to give standard normal
+GRAND(Rnorm, D, N, N, N, N, N) // special case: rnorm can take no args to give standard normal
 
-// just bernoulli?
+// just bernoulli? TODO make sure ints are returned (right now floats)
 #define RAND_bern(FUN) \
-GRAND(FUN, N, D, N, N, N) \
-GRAND(FUN, N, D, Seed, N, N) \
-GRAND(FUN, N, D, Eng, N, N) \
-GRAND(FUN, Mi, Dim, D, N, N)
+GRAND(FUN, I, N, D, N, N, N) \
+GRAND(FUN, I, N, D, Seed, N, N) \
+GRAND(FUN, I, N, D, Eng, N, N) \
+GRAND(FUN, Mi, Mi, Dim, D, N, N)
 
 #define RAND1_d(FUN) \
-GRAND(FUN, N, D, N, N, N) \
-GRAND(FUN, N, D, Seed, N, N) \
-GRAND(FUN, N, D, Eng, N, N) \
-GRAND(FUN, M, Dim, D, N, N)
+GRAND(FUN, D, N, D, N, N, N) \
+GRAND(FUN, D, N, D, Seed, N, N) \
+GRAND(FUN, D, N, D, Eng, N, N) \
+GRAND(FUN, M, M, Dim, D, N, N)
 
-// just binomial?
+// just binomial? TODO make sure ints are returned (right now floats)
 #define RAND_binom(FUN) \
-GRAND(FUN, N, I, D, N, N) \
-GRAND(FUN, N, I, D, Seed, N) \
-GRAND(FUN, N, I, D, Eng, N) \
-GRAND(FUN, Mi, Dim, D, D, N)
+GRAND(FUN, I, N, I, D, N, N) \
+GRAND(FUN, I, N, I, D, Seed, N) \
+GRAND(FUN, I, N, I, D, Eng, N) \
+GRAND(FUN, Mi, Mi, Dim, I, D, N)
 
 #define RAND2_d(FUN) \
-GRAND(FUN, N, D, D, N, N) \
-GRAND(FUN, N, D, D, Seed, N) \
-GRAND(FUN, N, D, D, Eng, N) \
-GRAND(FUN, M, Dim, D, D, N)
+GRAND(FUN, D, N, D, D, N, N) \
+GRAND(FUN, D, N, D, D, Seed, N) \
+GRAND(FUN, D, N, D, D, Eng, N) \
+GRAND(FUN, M, M, Dim, D, D, N)
 
+// final step of C++ bit-- generate all fns
 RAND2_d(Rnorm)
 RAND_bern(Rbern)
 RAND2_d(Rbeta)
+RAND_binom(Rbinom)
+RAND2_d(Rcauchy)
 
 // ----------------
 // Python interface
@@ -99,26 +114,38 @@ RAND2_d(Rbeta)
 #define Types2i_3 Types2i_1, rand_engine_t&
 #define Types2i_4 const uint64_t, const uint64_t, Types2i_1
 
+#define Doc1 "Random numbers from the "
+#define Doc2 " distribution."
 #define PybindDefs_1d(FUN, funstr, parname1, docstring) \
-m.def(funstr, py::overload_cast<Types1d_1>(&fn##FUN), docstring, py::arg(parname1)); \
+m.def(funstr, py::overload_cast<Types1d_1>(&fn##FUN), Doc1 docstring Doc2, py::arg(parname1)); \
 m.def(funstr, py::overload_cast<Types1d_2>(&fn##FUN), "", py::arg(parname1), "seed_val"_a); \
 m.def(funstr, py::overload_cast<Types1d_3>(&fn##FUN), "", py::arg(parname1), "engine"_a); \
 m.def(funstr, py::overload_cast<Types1d_4>(&fn##FUN), "", "n"_a, "k"_a, py::arg(parname1));
 
 #define PybindDefs_2d(FUN, funstr, parname1, parname2, docstring) \
-m.def(funstr, py::overload_cast<Types2d_1>(&fn##FUN), docstring, py::arg(parname1), py::arg(parname2)); \
+m.def(funstr, py::overload_cast<Types2d_1>(&fn##FUN), Doc1 docstring Doc2, py::arg(parname1), py::arg(parname2)); \
 m.def(funstr, py::overload_cast<Types2d_2>(&fn##FUN), "", py::arg(parname1), py::arg(parname2), "seed_val"_a); \
 m.def(funstr, py::overload_cast<Types2d_3>(&fn##FUN), "", py::arg(parname1), py::arg(parname2), "engine"_a); \
 m.def(funstr, py::overload_cast<Types2d_4>(&fn##FUN), "", "n"_a, "k"_a, py::arg(parname1), py::arg(parname2));
+
+#define PybindDefs_2i(FUN, funstr, parname1, parname2, docstring) \
+m.def(funstr, py::overload_cast<Types2i_1>(&fn##FUN), Doc1 docstring Doc2, py::arg(parname1), py::arg(parname2)); \
+m.def(funstr, py::overload_cast<Types2i_2>(&fn##FUN), "", py::arg(parname1), py::arg(parname2), "seed_val"_a); \
+m.def(funstr, py::overload_cast<Types2i_3>(&fn##FUN), "", py::arg(parname1), py::arg(parname2), "engine"_a); \
+m.def(funstr, py::overload_cast<Types2i_4>(&fn##FUN), "", "n"_a, "k"_a, py::arg(parname1), py::arg(parname2));
 
 namespace py = pybind11;
 using namespace pybind11::literals;
 
 void rand(py::module &m)
 {
-  m.def("rnorm", py::overload_cast<>(&rnorm), "Random numbers the Normal (Gaussian) distribution.");
+  m.def("rnorm", py::overload_cast<>(&rnorm), "Random numbers from the Normal (Gaussian) distribution.");
   PybindDefs_2d(Rnorm, "rnorm", "mu", "sigma", "") // no docstring
-  PybindDefs_1d(Rbern, "rbern", "p", "Random numbers from the Bernoulli distribution.")
+  PybindDefs_1d(Rbern, "rbern", "prob", "Bernoulli")
+  PybindDefs_2d(Rbeta, "rbeta", "a", "b", "Beta")
+  PybindDefs_2i(Rbinom, "rbinom", "n_trials", "prob", "Binomial")
+
+  //m.def("rpois", py::overload_cast<const double, const uint64_t>(&stats::rpois));
 
   py::class_<std::mt19937_64>(m, "Engine")
       .def(py::init<int>());
