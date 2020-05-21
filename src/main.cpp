96ce41312065339d643aaea1e82ac3c29f7c859d
Alex Forrence <alex.forrence@gmail.com>
2020-05-20 22:59:24 -0400

    add prob, and whispers of test

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,12 @@ namespace py = pybind11;
 
 void rand(py::module&);
 void dens(py::module&);
+void prob(py::module&);
 
 PYBIND11_MODULE(statslib, m)
 {
   m.doc() = "pybind11 wrapper for kthohr/statslib";
   dens(m);
   rand(m);
+  prob(m);
 }
