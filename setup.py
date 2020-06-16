bd799a8dd746ca4c918853fce8f615e8804bcb6e
Alex Forrence <alex.forrence@gmail.com>
2020-06-16 16:44:53 -0400

    fix source dist?

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='statslib',
-    version='0.0.2a2',
+    version='0.0.2a3',
     author='Alexander Forrence',
     author_email='alex.forrence@gmail.com',
     description='Bindings for StatsLib (https://github.com/kthohr/stats)',
